7bec9cae8b0412e542c3062ad93c90708b646648
Edward Greenberg <ed.w.greenberg@gmail.com>
2019-10-10 13:52:03 -0400

    frontend

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ class Index extends React.Component {
     constructor(props){
         super(props);
         this.state = {value: '', output: false,
-        questions: ["What is the famous moon of Saturn? Answer the question for another secret question.","What is the famous moon of Pluto?","Both correct."],
+        questions: ["What is the famous moon of Saturn? Answer the question for another secret question.",
+        "What is the famous moon of Pluto?",
+        "Both correct."],
     };
 
         this.handleChange = this.handleChange.bind(this);
@@ -21,16 +23,29 @@ class Index extends React.Component {
         event.preventDefault();
       }
 
+    componentDidMount(){
+      fetch("http://localhost:3434/student/mo")
+      .then(res => res.json())
+      .then(student => {
+        console.log(student)
+      }
+
+      )
+    }
+
     render(){
+      console.log('bi')
   return (
       <div>
-        <div>Welcome to questions! Type 'question' to get started.</div>
+        <div>Welcome to quesfdstions! Type 'question' to get started.</div>
         <form onSubmit={this.handleSubmit}>
-        <label>Name: <input type="text" value={this.state.value} onChange={this.handleChange} />
+        <label>Answer: <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        {/* <input type="submit" value="Submit" /> */}
+        <input type="submit" value="Submit" />
       </form>
-      <div>{this.state.value == "question" ? this.state.questions[0] : this.state.value == "Titan" ? this.state.questions[1] : this.state.value == "Charon" ? this.state.questions[2]: "" }</div>
+      <div>{this.state.value == "question" ? this.state.questions[0] :
+       this.state.value == "Titan" ? this.state.questions[1] :
+        this.state.value == "Charon" ? this.state.questions[2]: "" }</div>
       </div>
       
     )
